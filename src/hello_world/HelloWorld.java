30f0ea80e07e64317da4a245872b62ec397461c3
o.chmykh <o.chmykh@vinzab-note17.mhp.com.ua.inc>
2018-10-25 14:46:32 +0300

    Another useless commit yet.

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("I love GOD! I really want to go back home and to be succesful.");
+		System.out.println("I love my family.");
 	}
 
 }
