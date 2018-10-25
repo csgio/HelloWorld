3d7328f4e302eba6ba65c1ecef143aaa9166a3e9
o.chmykh <o.chmykh@vinzab-note17.mhp.com.ua.inc>
2018-10-25 14:49:38 +0300

    The comment has been deleted.

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -3,7 +3,6 @@ package hello_world;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("I love GOD! I really want to go back home and to be succesful.");
 		System.out.println("I love my family.");
