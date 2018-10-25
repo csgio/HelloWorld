4b8e03c95bad279d670f03e9d777be50b9065e25
o.chmykh <o.chmykh@vinzab-note17.mhp.com.ua.inc>
2018-10-25 14:12:50 +0300

    The third commit is also useless.

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("I love GOD! I really want to go back home and to be succesful.");
 	}
 
 }
